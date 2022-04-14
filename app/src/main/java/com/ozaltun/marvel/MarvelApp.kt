d6e89385953caa9f7020ec2e17051ba462fdca2c
sezer1578 <sezer_ozaltin@hotmail.com>
2022-04-14 10:02:47 +0300

    final

--- app/src/main/java/com/ozaltun/marvel/MarvelApp.kt
+++ app/src/main/java/com/ozaltun/marvel/MarvelApp.kt
@@ -1,17 +1,18 @@
 package com.ozaltun.marvel
 
 import android.app.Application
-import com.ozaltun.marvel.di.marvelModule
+import com.ozaltun.marvel.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-class MarvelApp : Application() {
+class MarvelApp : Application(){
 
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR)
             androidContext(this@MarvelApp)
             modules(
                 listOf(
